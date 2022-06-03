235111a8023accba47ab193f270481e26820cb76
unknown <zulumkhuzo@gmail.com>
2022-06-03 17:11:30 +0800

    clear folders

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const cors = require('cors')
-const {createPaymentIntent} = require("./controllers/stripe")
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
@@ -13,35 +12,24 @@ app.use(morgan("dev"))
 app.use(bodyParser.json({limit:"2mb"}))
 app.use(cors())
 
-// app.use("/api", authRoutes)
-// fs.readdirSync("./routes").map(r => app.use("/api",require("./routes/" + r)));
-// app.get("/create-payment-intent", (req, res) => {
-//     res.send({
-//         create:"create - payment"
-//     })
-// })
-app.get("/create-payment-intent", createPaymentIntent)
-app.post("/create-payment-intent", createPaymentIntent)
-
 app.get("/", (req, res) => {
     res.send({
         send:"yessir"
     })
 })
-
-app.post("/payments/create", async (req, res) => {
-    // const total = req.query.total
-
-    // console.log("total boi -->", total);
-
+app.post("/create-payment-intent", async (req, res) => {
+    const total = req.body
+    let _total = total.total/20
+   
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: 100*100,
-        currency: "usd",
-    })
+        amount:_total * 100,
+        currency:"usd"
+    });
 
-    res.status(201).send({
+    res.send({
         clientSecret: paymentIntent.client_secret,
     })
+    
 })
 
 const port = process.env.PORT || 8000;
--- routes/stripe.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { createPaymentIntent } = require('../controllers/stripe')
-
-const router = express.Router()
-
-router.post("/create-payment-intent",createPaymentIntent)
-
-module.exports = router
